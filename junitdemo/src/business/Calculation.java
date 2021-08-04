951ff576692458e06ebadc8d372dea5768930cbb
Prateek Sharma <arryprateek@gmail.com>
2021-08-04 16:52:52 +0530

    Junit Commit 1

---
+++ junitdemo/src/business/Calculation.java
@@ -0,0 +1,30 @@
+package business;
+
+public class Calculation 
+{
+	
+		public double add(double a, double b) 
+		{
+        return a + b;
+       }
+       
+       public double subtract(double a, double b) 
+       {
+        return a - b;
+       }
+       
+       public double multiply(double a, double b) 
+       {
+        return a * b;
+       }
+       
+       public double divide(double a, double b)
+       {
+        if(b == 0) 
+        {
+         throw new ArithmeticException();
+        }
+        return a / b;
+       }
+
+}
---
+++ junitdemo/src/business/JunitMessage.java
@@ -0,0 +1,19 @@
+package business;
+
+public class JunitMessage {
+	String m;
+	public JunitMessage(String msg)
+	{
+		m=msg;
+	}
+	public String printMessage()
+	{
+		return m;
+	}
+
+	public String printHiMessage()
+	{
+		return "Hi "+m;
+	}
+
+}
